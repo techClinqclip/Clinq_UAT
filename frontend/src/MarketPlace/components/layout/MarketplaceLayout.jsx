9e2fb2a19466f2b0aa1a32853be06ca965bdc656
Pritam <kumarpreetjangir@gmail.com>
2026-09-16 01:39:12 +0530

    rigth sidebar

--- frontend/src/MarketPlace/components/layout/MarketplaceLayout.jsx
+++ frontend/src/MarketPlace/components/layout/MarketplaceLayout.jsx
@@ -1,35 +1,33 @@
 import AppSidebar from "../../../components/navigation/AppSidebar";
 import RightSidebar from "../rightSidebar/RightSidebar";
 import TopNavbar from "../../../shared/navbar/TopNavbar";
-import { useState } from "react";
+
 export default function MarketplaceLayout({
   children,
   role = "creator",
   campaigns = [],
 }) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
     <div className="min-h-screen bg-zinc-950">
-      <AppSidebar
-        role={role}
-        mobileOpen={sidebarOpen}
-        onMobileClose={() => setSidebarOpen(false)}
-      />
+      <AppSidebar />
 
       <main className="ml-0 lg:ml-64">
-        <TopNavbar onMenuClick={() => setSidebarOpen(true)} />
+        <TopNavbar role={role} />
 
-        <div className="flex gap-8 px-4 py-6 sm:px-6 lg:px-8 lg:py-8">
+        <div className="flex flex-col gap-8 px-4 py-6 sm:px-6 sm:py-8 lg:px-8 xl:flex-row">
+          
+          {/* Main marketplace content */}
           <div className="min-w-0 flex-1">
             {children}
           </div>
 
-          <aside className="hidden w-80 shrink-0 xl:block">
+          {/* Right sidebar */}
+          <aside className="w-full shrink-0 xl:w-80">
             <div className="sticky top-24 max-h-[calc(100vh-6rem)] overflow-y-auto custom-scrollbar">
               <RightSidebar campaigns={campaigns} />
             </div>
           </aside>
+
         </div>
       </main>
     </div>
